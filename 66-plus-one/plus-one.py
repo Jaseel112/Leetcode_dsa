04e4231257641f76f881aff5f60039c5022ff03c
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-02-29 14:57:27 +0530

    Time: 39 ms (44.52%) | Memory: 16.7 MB (35.56%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -9,16 +9,7 @@ class Solution:
         n+=digits[k-1]
         n+=1
         mylist=[n]
-        output= list(map(int, str(mylist[0])))
-        for i in range (len(output)):
-            if i<len(digits):
-                m=output[i]
-                print(m)
-                digits[i]=m
-            else:
-                m=output[i]
-                print(m)
-                digits.append(m)
+        digits= list(map(int, str(mylist[0])))
         return digits
         
         
