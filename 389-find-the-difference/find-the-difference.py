67362d9b369202bd51f18135eaf5c8dfc81c9faf
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-12 19:32:42 +0530

    Time: 38 ms (59.69%) | Memory: 16.6 MB (36.05%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,9 +1,12 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s1=sorted(s)
-        t1=sorted(t)
-        n=len(s)
-        for i in range (n):
-            if s1[i]!=t1[i]:
-                return t1[i]
-        return t1[n]
+        # s1=sorted(s)
+        # t1=sorted(t)
+        # n=len(s)
+        # for i in range (n):
+        #     if s1[i]!=t1[i]:
+        #         return t1[i]
+        # return t1[n]
+        s1=Counter(s)
+        t1=Counter(t)
+        return list((t1-s1).elements())[0]
