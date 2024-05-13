410e8c55ca5be168d7410e151fe0bd2bc5b20402
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-13 13:04:10 +0530

    Time: 229 ms (87.67%) | Memory: 18.6 MB (34.27%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -3,6 +3,6 @@ class Solution:
         map=Counter(nums)
         ln=0
         for i,count in map.items():
-            if i+1 in nums:
+            if i+1 in map:
                 ln=max(ln,count+map[i+1])
         return ln
