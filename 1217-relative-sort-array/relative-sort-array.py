1bbeaab766a7d2539efa23487ef8f073b7402dcb
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-13 17:47:44 +0530

    Time: 36 ms (82.61%) | Memory: 16.8 MB (37.94%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,9 +1,15 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        # out=[]
+        # for i in arr2:
+        #     while i in arr1:
+        #         arr1.remove(i)
+        #         out.append(i)
+        # out+=sorted(arr1)
+        # return out
+        c=Counter(arr1)
         out=[]
         for i in arr2:
-            while i in arr1:
-                arr1.remove(i)
-                out.append(i)
-        out+=sorted(arr1)
+            out+=[i]*c.pop(i)
+        out+=sorted(c.elements())
         return out
