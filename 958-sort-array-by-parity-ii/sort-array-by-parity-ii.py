79840a327078681e02d9ae79e085b52b92ca2804
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-13 14:48:50 +0530

    Time: 160 ms (23.10%) | Memory: 19 MB (73.56%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -3,13 +3,7 @@ class Solution:
         nume=list(filter(lambda x:x%2==0,nums))
         numo=list(filter(lambda x:x%2!=0,nums))
         num=[]
-        j=0
-        k=0
-        for i in range(len(nums)):
-            if i%2==0:
-                num.append(nume[j])
-                j+=1
-            else:
-                num.append(numo[k])
-                k+=1
+        for i in range(len(nume)):
+            num.append(nume[i])
+            num.append(numo[i])
         return num
