ec4b4b2383cdfe618a9a40c128323987fe4cd02f
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-13 18:31:56 +0530

    Time: 74 ms (15.11%) | Memory: 16.7 MB (69.17%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
@@ -4,3 +4,11 @@ class Solution:
             nums.sort()
             nums[0]=-nums[0]
         return sum(nums)
+        # nums=sorted(nums)
+        # for i in range(k):
+        #     if k>0 and nums[i]<0:
+        #         nums[i]=-nums[i]
+        # nums=sorted(nums)
+        # if k>0 and k%2!=0:
+        #     nums[0]=-nums[0]
+        # return sum(nums)
