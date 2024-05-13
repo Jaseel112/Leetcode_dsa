3c10970405028cd4418cec2ac9188f0d20753df4
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-13 14:52:09 +0530

    Time: 142 ms (92.63%) | Memory: 18.8 MB (98.71%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -1,9 +1,20 @@
 class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        nume=list(filter(lambda x:x%2==0,nums))
-        numo=list(filter(lambda x:x%2!=0,nums))
-        num=[]
-        for i in range(len(nume)):
-            num.append(nume[i])
-            num.append(numo[i])
+        # nume=list(filter(lambda x:x%2==0,nums))
+        # numo=list(filter(lambda x:x%2!=0,nums))
+        # num=[]
+        # for i in range(len(nume)):
+        #     num.append(nume[i])
+        #     num.append(numo[i])
+        # return num
+        j=0
+        k=1
+        num=[0]*len(nums)
+        for i in nums:
+            if i%2==0:
+                num[j]=i
+                j+=2
+            else:
+                num[k]=i
+                k+=2
         return num
