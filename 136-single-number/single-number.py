debe08fe9d80ad984901d50f1113a6d5b557dc2b
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-14 15:03:36 +0530

    Time: 104 ms (85.78%) | Memory: 19.2 MB (33.80%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -2,4 +2,7 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         n=len(nums)
         c=Counter(nums)
-        return c.most_common(n)[-1][0]
+        # return c.most_common(n)[-1][0]
+        for i,j in c.items():
+            if j==1:
+                return i
