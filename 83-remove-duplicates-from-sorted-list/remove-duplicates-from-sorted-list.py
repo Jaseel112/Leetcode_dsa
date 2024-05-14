6c63ceaf2f19e29cb43861d8a317643df725de23
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-05-14 14:50:09 +0530

    Time: 43 ms (34.11%) | Memory: 16.5 MB (63.06%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -5,15 +5,27 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        map=[]
-        temp=head
-        prev=head
-        while temp:
-            if temp.val not in map:
-                map.append(temp.val)
-                prev=temp
-            elif temp.val in map:
-                prev.next=temp.next
-            temp=temp.next
+        # map=[]
+        # temp=head
+        # prev=head
+        # while temp:
+        #     if temp.val not in map:
+        #         map.append(temp.val)
+        #         prev=temp
+        #     elif temp.val in map:
+        #         prev.next=temp.next
+        #     temp=temp.next
 
-        return head
+        # return head
+        if head==None:
+            return head
+        else:
+            temp=head.next
+            prev=head
+            while temp:
+                if prev.val==temp.val:
+                    prev.next=temp.next
+                else:
+                    prev=temp
+                temp=temp.next
+            return head
