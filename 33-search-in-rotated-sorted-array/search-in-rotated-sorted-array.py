849eccb3cd86f96bc49e2dd9ab5a001f2a42b9cf
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-07-23 15:33:56 +0530

    Time: 36 ms (92.10%) | Memory: 17 MB (27.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,50 +1,50 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        find=nums.copy()
-        find.sort()
-        n=nums[0]
-        low=0
-        high=len(find)-1
-        k=0
-        while(low<=high):
-            mid=(low+high)//2
-            if find[mid]==n:
-                k=mid
-                break
-            elif find[mid]>n:
-                high=mid-1
-            else:
-                low=mid+1
-        if target>=nums[0]:
-            low=0
-            high=len(nums)-k-1
-        if target<nums[0]:
-            low=len(nums)-k
-            high=len(nums)-1
-        while(low<=high):
-            mid=(low+high)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[mid]>target:
-                high=mid-1
-            else:
-                low=mid+1
-        return -1
+        # find=nums.copy()
+        # find.sort()
+        # n=nums[0]
         # low=0
-        # high=len(nums)-1
-        # while low<=high:
+        # high=len(find)-1
+        # k=0
+        # while(low<=high):
+        #     mid=(low+high)//2
+        #     if find[mid]==n:
+        #         k=mid
+        #         break
+        #     elif find[mid]>n:
+        #         high=mid-1
+        #     else:
+        #         low=mid+1
+        # if target>=nums[0]:
+        #     low=0
+        #     high=len(nums)-k-1
+        # if target<nums[0]:
+        #     low=len(nums)-k
+        #     high=len(nums)-1
+        # while(low<=high):
         #     mid=(low+high)//2
         #     if nums[mid]==target:
         #         return mid
-        #     elif nums[low]<=nums[mid]:
-        #         if nums[low]<=target<nums[mid]:
-        #             high=mid-1
-        #         else:
-        #             low=mid+1
+        #     elif nums[mid]>target:
+        #         high=mid-1
         #     else:
-        #         if nums[mid]<target<=nums[high]:
-        #             low=mid+1
-        #         else:
-        #             high=mid-1
+        #         low=mid+1
         # return -1
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[low]<=nums[mid]:
+                if nums[low]<=target<nums[mid]:
+                    high=mid-1
+                else:
+                    low=mid+1
+            else:
+                if nums[mid]<target<=nums[high]:
+                    low=mid+1
+                else:
+                    high=mid-1
+        return -1
 
