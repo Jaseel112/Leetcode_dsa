a0de5c789f6a179d433f5a67712282e8b7c8c159
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-07-26 19:49:28 +0530

    Time: 29 ms (87.67%) | Memory: 16.5 MB (13.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,17 +1,29 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        hashmap={}
-        def backtrack(steps):
-            if steps in hashmap:
-                return hashmap[steps]
-            if steps==0:
-                return 1
-            if steps<0:
-                return 0
-            hashmap[steps]= backtrack(steps-1)+backtrack(steps-2)
-            return hashmap[steps]
+
+        # #memoisation technique
+        # hashmap={}
+        # def backtrack(steps):
+        #     if steps in hashmap:
+        #         return hashmap[steps]
+        #     if steps==0:
+        #         return 1
+        #     if steps<0:
+        #         return 0
+        #     hashmap[steps]= backtrack(steps-1)+backtrack(steps-2)
+        #     return hashmap[steps]
         
-        return backtrack(n)
+        # return backtrack(n)
+
+        #more optimized code (fibanocci sequence)
+
+        s=[0,1]
+
+        for i in range(1,n+1):
+            s.append(s[i-1]+s[i])
+        return s[-1]
+
+
 
 
 
