46b372da63bb1a125b2808b76f92d3b530b24cd5
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-07-26 19:26:39 +0530

    Time: 42 ms (9.65%) | Memory: 16.4 MB (51.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @cache
+        hashmap={}
         def bfs(steps):
+            if steps in hashmap:
+                return hashmap[steps]
             if steps==0:
                 return 1
             if steps<0:
                 return 0
-            
-            return bfs(steps-1) + bfs(steps-2)
+            hashmap[steps]=bfs(steps-1) + bfs(steps-2)
+            return hashmap[steps]
         return bfs(n)
 
 
