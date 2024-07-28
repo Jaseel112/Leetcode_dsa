1d75600a77e219e9ac46fee60ec0799e53015b61
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-07-28 21:51:13 +0530

    Time: 35 ms (84.48%) | Memory: 16.6 MB (76.54%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,21 +1,40 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # s=0
-        # for i in wordDict:
-        @cache
-        def dfs(ch,i):
-            if i==len(s)-1:
-                if ch in wordDict:
-                    return True
-                else:
-                    return False
-            else:
-                if ch in wordDict:
-                    return (dfs(s[i+1],i+1) or dfs(ch+s[i+1],i+1))
-                else:
-                    return dfs(ch+s[i+1],i+1)
+        # # s=0
+        # # for i in wordDict:
+        # @cache
+        # def dfs(ch,i):
+        #     if i==len(s)-1:
+        #         if ch in wordDict:
+        #             return True
+        #         else:
+        #             return False
+        #     else:
+        #         if ch in wordDict:
+        #             return (dfs(s[i+1],i+1) or dfs(ch+s[i+1],i+1))
+        #         else:
+        #             return dfs(ch+s[i+1],i+1)
             
-        return dfs(s[0],0)
+        # return dfs(s[0],0)
+
+        def f(index):
+            if index==len(s):
+                return  True
+            if dp[index]!=None:
+                return dp[index]
+            for i in wordDict:
+                if s[index:index+len(i)]==i:
+                    if f(index+len(i)):
+                        dp[index]=True
+                        return True
+            dp[index]=False
+            return False
+        dp=[None for _ in range(len(s))]
+        return f(0)
+
+            
+            
+            
             
             
             
