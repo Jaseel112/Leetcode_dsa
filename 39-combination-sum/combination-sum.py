20f74c67b52c2dd30f90fe32c2ce5ec60a6ac648
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-07-28 23:37:42 +0530

    Time: 52 ms (60.82%) | Memory: 16.7 MB (10.62%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,35 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # def check(start,path,target):
+        #     if target==0:
+        #         res.append(path)
+        #         return
+        #     for i in range(start,len(candidates)):
+        #         if target-candidates[i]>=0:
+        #             check(i,path+[candidates[i]],target-candidates[i])
+        # res=[]
+        # check(0,[],target)
+        # return res
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        def f(sum1,comb,j):
+            if sum1==0:
+                dp.append(comb)
+                return
+            for i in range(j,len(candidates)):
+                if sum1-candidates[i]>=0:
+                    f(sum1-candidates[i],comb+[candidates[i]],i)
+        dp=[]
+        f(target,[],0)
+        return dp
