1dcb08737b06ae2c1ccd64e75f165ddebf2cfe54
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-07-29 00:13:01 +0530

    Time: 345 ms (78.18%) | Memory: 17.9 MB (33.69%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,13 +1,20 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        @cache
-        def f(index):
-            if index>=len(nums)-1:
-                return True           
-            fl=False
-            for i in range(1,nums[index]+1):
-                 fl=fl or f(index+i)
-                 if fl==True:
-                    break
-            return fl
-        return f(0)
+        # @cache
+        # def f(index):
+        #     if index>=len(nums)-1:
+        #         return True           
+        #     fl=False
+        #     for i in range(1,nums[index]+1):
+        #          fl=fl or f(index+i)
+        #          if fl==True:
+        #             break
+        #     return fl
+        # return f(0)
+
+        goal=len(nums)-1
+
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i]>=goal:
+                goal=i
+        return goal==0
