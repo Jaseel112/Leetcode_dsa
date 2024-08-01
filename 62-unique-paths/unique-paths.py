94d365b28b450a0aab1697cc62e11e162f0566ad
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-08-01 21:20:06 +0530

    Time: 38 ms (39.86%) | Memory: 16.6 MB (30.86%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,11 +1,28 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        aboveRow = [1] * n
+        # mp={}
+        # def f(i,j):
+        #     print(i,j)
+        #     if (i,j) in mp:
+        #         return mp[(i,j)]
+        #     if i==m-1 and j==n-1:
+        #         return 1
+        #     if j>=m or i>=n:
+        #         return 0
+        #     a=f(i,j+1)            
+        #     b=f(i+1,j)
+        #     mp[(i,j)]=a+b
+        #     return mp[(i,j)]
+        # return f(0,0)
+
+
+        abrow=[1]*n
 
         for _ in range(m-1):
-            currentRow = [1] * n
-            for i in range(1, n):
-                currentRow[i] = currentRow[i-1] + aboveRow[i]
-            aboveRow = currentRow
+            curr=[1]*n
+            for i in range(1,n):
+                curr[i]=abrow[i]+curr[i-1]
+            abrow=curr
         
-        return aboveRow[-1]
+        return abrow[-1]
+
