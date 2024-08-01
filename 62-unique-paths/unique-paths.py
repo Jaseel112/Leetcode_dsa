bde8569a3edb5c38313c45cea156636608aced1a
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-08-01 21:28:04 +0530

    Time: 44 ms (7.47%) | Memory: 16.9 MB (8.35%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,28 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # mp={}
-        # def f(i,j):
-        #     print(i,j)
-        #     if (i,j) in mp:
-        #         return mp[(i,j)]
-        #     if i==m-1 and j==n-1:
-        #         return 1
-        #     if j>=m or i>=n:
-        #         return 0
-        #     a=f(i,j+1)            
-        #     b=f(i+1,j)
-        #     mp[(i,j)]=a+b
-        #     return mp[(i,j)]
-        # return f(0,0)
+        mp={}
+        def f(i,j):
+            if (i,j) in mp:
+                return mp[(i,j)]
+            if i==n-1 and j==m-1:
+                return 1
+            if j>=m or i>=n:
+                return 0
+            a=f(i,j+1)            
+            b=f(i+1,j)
+            mp[(i,j)]=mp.get((i,j),0)+a+b
+            return mp[(i,j)]
+        return f(0,0)
 
 
-        abrow=[1]*n
+        # abrow=[1]*n
 
-        for _ in range(m-1):
-            curr=[1]*n
-            for i in range(1,n):
-                curr[i]=abrow[i]+curr[i-1]
-            abrow=curr
+        # for _ in range(m-1):
+        #     curr=[1]*n
+        #     for i in range(1,n):
+        #         curr[i]=abrow[i]+curr[i-1]
+        #     abrow=curr
         
-        return abrow[-1]
+        # return abrow[-1]
 
