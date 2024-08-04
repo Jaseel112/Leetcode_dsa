0f8e11229970d0033d10464465f297c39cc1328a
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-08-04 13:10:42 +0530

    Time: 38 ms (44.46%) | Memory: 16.5 MB (48.48%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,24 +1,40 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp={}
-        if s[0]=="0":
-            return 0
-        if len(s)==1:
-            return 1
-        def f(i):
-            if i>=len(s):
-                return 1
-            if s[i]=='0':
-                return 0
-            if i in dp:
-                return dp[i]
-            take=f(i+1)
-            not_take=0
-            if i+1<=len(s)-1 and int(s[i:i+2])<27:
-                not_take=f(i+2)
-            dp[i]=take+not_take
-            return dp[i]
+        # dp={}
+        # if s[0]=="0":
+        #     return 0
+        # if len(s)==1:
+        #     return 1
+        # def f(i):
+        #     if i>=len(s):
+        #         return 1
+        #     if s[i]=='0':
+        #         return 0
+        #     if i in dp:
+        #         return dp[i]
+        #     take=f(i+1)
+        #     not_take=0
+        #     if i+1<=len(s)-1 and int(s[i:i+2])<27:
+        #         not_take=f(i+2)
+        #     dp[i]=take+not_take
+        #     return dp[i]
         
-        return f(0)
+        # return f(0)
+        if s[0]=='0':
+            return 0
+        dp=[0]*(len(s)+1)
+        dp[-1]=1
+        for i in range(len(s)-1,-1,-1):
+            if s[i]!='0':
+                if i==len(s)-1:
+                    dp[i]=dp[i+1]
+                else:
+                    if int(s[i:i+2])<27:
+                        dp[i]=dp[i+1]+dp[i+2]
+                    else:
+                        dp[i]=dp[i+1]
+        return dp[0]
+            
+
                 
             
