14a69875ededcb023e073509244640ac719e48ed
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-08-04 21:42:30 +0530

    Time: 56 ms (7.92%) | Memory: 19.4 MB (16.60%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,22 +6,22 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # map={}
-        # temp=head
-        # while temp:
-        #     if temp not in map:
-        #         map[temp]=1
-        #     elif temp in map:
-        #         return True
-        #     temp=temp.next
-        # return False
-
-        hare=head
-        tort=head
-
-        while hare and hare.next:
-            tort=tort.next if tort else None
-            hare=hare.next.next if hare and hare.next else None
-            if hare==tort:
+        map={}
+        temp=head
+        while temp:
+            if temp not in map:
+                map[temp]=1
+            elif temp in map:
                 return True
+            temp=temp.next
         return False
+
+        # hare=head
+        # tort=head
+
+        # while hare and hare.next:
+        #     tort=tort.next if tort else None
+        #     hare=hare.next.next if hare and hare.next else None
+        #     if hare==tort:
+        #         return True
+        # return False
