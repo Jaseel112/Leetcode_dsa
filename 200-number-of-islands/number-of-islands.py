7f4ebfa2b8181bc096fd2de6e54651072ec6e1d5
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-08-06 14:18:25 +0530

    Time: 310 ms (14.89%) | Memory: 24.3 MB (21.38%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count=0
+        if not grid:
+            return 0
+        row,column=len(grid),len(grid[0])
+        q=[]
+        visited=set()
+        def bfs(r,c):
+            q.append((r,c))
+            visited.add((r,c))
+            dire=[[-1,0],[0,-1],[1,0],[0,1]]
+            while q:
+                r,c=q.pop(0)
+                for d in dire:
+                    i,j=r+d[0],c+d[1]
+                    if i in range(row) and j in range(column) and grid[i][j]=="1" and (i,j) not in visited:
+                        q.append((i,j))
+                        visited.add((i,j))
+
+        for i in range(row):
+            for j in range(column):
+                if grid[i][j]=="1" and (i,j) not in visited:
+                    bfs(i,j)
+                    count+=1
+        return count
+
+        
