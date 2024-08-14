53829411c690c0ffa80859ab4ce6e0e58fc34894
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-08-14 23:35:07 +0530

    Time: 65 ms (13.79%) | Memory: 24.5 MB (95.27%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,25 +8,38 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        i=0
-        j=0
-        start=head
-        end=head.next
-        while end!=None:
-            j+=1
-            end=end.next
-        while start.next:
-            end=start
-            prev=None
-            while end.next:
-                prev=end
-                end=end.next 
-            if j-i<=1:
-                break           
-            end.next=start.next
-            start.next=end  
-            prev.next=None   
-            start=end.next          
-            i+=1
-            j-=1   
-        return head
+        # i=0
+        # j=0
+        # start=head
+        # end=head.next
+        # while end!=None:
+        #     j+=1
+        #     end=end.next
+        # while start.next:
+        #     end=start.next
+        #     prev=start
+        #     while end.next:
+        #         prev=end
+        #         end=end.next 
+        #     if j-i<=1:
+        #         break           
+        #     end.next=start.next
+        #     start.next=end  
+        #     prev.next=None   
+        #     start=end.next          
+        #     i+=1
+        #     j-=1   
+        # return head
+
+        if not head: return
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        prev, curr = None, slow.next
+        slow.next = None
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        first, second = head, prev
+        while second:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
