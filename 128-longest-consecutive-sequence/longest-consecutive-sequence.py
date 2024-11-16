f4e3b18f6fea41611c03aec0d90270fdc0c6f6da
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2024-11-16 20:37:47 +0530

    Time: 99 ms (46.43%) | Memory: 56.8 MB (5.03%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,18 +1,36 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+#         if len(nums)==0:
+#             return 0
+#         nums.sort()
+#         res=0
+#         count=1
+#         for i in range(1,len(nums)):
+#             if nums[i]==nums[i-1]+1:
+#                 count+=1
+#             elif nums[i]==nums[i-1]:
+#                 continue
+#             else:
+#                 res=max(res,count)
+#                 count=1
+#         res=max(res,count)
+#         return res
+                
+
+
+
         if len(nums)==0:
             return 0
         nums.sort()
-        res=0
-        count=1
-        for i in range(1,len(nums)):
-            if nums[i]==nums[i-1]+1:
-                count+=1
-            elif nums[i]==nums[i-1]:
-                continue
+        def backtracking(i,prev,result):
+            if i>=len(nums):
+                return result
+            l=r=result
+            if nums[i]==prev+1:
+                l=backtracking(i+1,nums[i],result+1)
+            elif nums[i]==prev:
+                l=backtracking(i+1,nums[i],result)
             else:
-                res=max(res,count)
-                count=1
-        res=max(res,count)
-        return res
-                
+                r=backtracking(i+1,nums[i],1)
+            return max(l,r)
+        return backtracking(1,nums[0],1)
