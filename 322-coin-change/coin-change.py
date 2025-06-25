93877ae74840e2bcbf57b8a16a39c699b49d32a0
Mohammed Jaseel C <128475477+Jaseel112@users.noreply.github.com>
2025-06-25 20:07:24 +0530

    Time: 771 ms (52.10%) | Memory: 18.2 MB (68.84%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -8,4 +8,27 @@ class Solution:
                     dp[i] = min(dp[i-x]+1,dp[i])
         return dp[amount] if dp[amount]!=amount+1 else -1
 
+
+        # hashmap={}
+        # result=[amount+1]
+        # flag=[0]
+        # if amount==0:
+        #     return 0
+        
+        # def backtrack(amount,count):
+        #     if amount==0:
+        #         flag[0]=1
+        #         result[0]=min(result[0],count)
+        #         return 
+        #     if amount<0:
+        #         return 
+        #     for i in range(len(coins)):
+        #             count+=1
+        #             backtrack(amount-coins[j],count)
+        #             count-=1
+        # backtrack(amount,0)
+        # return result[0] if flag[0] else -1
+
+
+
             
